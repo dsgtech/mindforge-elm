62540cb965abfe2c787d44e1280f00b0dc7672b8
Sandeep Datta <datta.sandeep@gmail.com>
2017-04-19 21:37:32 +0530

    The user can now select from a list of available input files. This almost works except for an unknown json bug related to encoding quotes.

--- Main.elm
+++ Main.elm
@@ -1,5 +1,5 @@
 import Html exposing (..)
-import Html.Attributes exposing (style, type_, placeholder)
+import Html.Attributes exposing (..)
 import Html.Events exposing (onClick, onInput)
 import Http
 import Json.Decode as JD
@@ -22,27 +22,29 @@ type Tree
 
 type alias Model =
     { tree : Tree
-    , fileName : String
+    -- , fileName : String
+    , fileList : List String
     }
 
 init : (Model, Cmd Msg)
 init =
-    (Model Empty "test0.json", getJson "test0.json")
+    (Model Empty [], listFiles)
 
 -- UPDATE
 
 type Msg
-    = Refresh
-    | JsonReceived (Result Http.Error (List JsonNode))
+    -- = Refresh
+    = JsonReceived (Result Http.Error (List JsonNode))
     | RebuildSource
     | RebuildSourceAck (Result Http.Error Int)
-    | MindMapFileName String
+    | FileSelected String
+    | FileList (Result Http.Error (List String))
 
 update : Msg -> Model -> (Model, Cmd Msg)
 update msg model =
     case msg of
-        Refresh ->
-            (model, getJson model.fileName)
+        -- Refresh ->
+        --     (model, getJson (Just model.fileName))
         JsonReceived (Ok jsonNodes) ->
             ({ model | tree = jsonNodes |> getTree }, Cmd.none)
         JsonReceived (Err _) -> -- TODO: Handle error properly.
@@ -51,8 +53,13 @@ update msg model =
             (model, rebuildSource)
         RebuildSourceAck (_) -> -- TODO: Handle error properly.
             (model, Cmd.none)
-        MindMapFileName name ->
-            ({ model | fileName = name }, Cmd.none)
+        FileSelected selectedFile ->
+            (model, getJson (Just selectedFile))
+        FileList (Ok fileList) ->
+            ({ model | fileList = fileList }, getJson (List.head fileList))
+        FileList (Err _) ->  -- TODO: Handle error properly.
+            (model, Cmd.none)
+
 
 -- VIEW
 view : Model -> Html Msg
@@ -60,12 +67,14 @@ view model =
     div []
     [ h1 [] [ text "MindForge" ]
     , button [onClick RebuildSource] [text "Rebuild Elm source"]
-    , input [ type_ "text"
-            , placeholder "mind map file"
-            , onInput MindMapFileName] []
-    , button [onClick Refresh] [text "Load"]
+    , select [onInput FileSelected] (List.map viewOption model.fileList)
+    -- , button [onClick Refresh] [text "Load"]
     , showTree model.tree
     ]
+
+viewOption : a -> Html msg
+viewOption val =
+    option [ value (toString val) ] [ text (toString val) ]
 
 showTree : Tree -> Html msg
 showTree tree =
@@ -113,10 +122,17 @@ getTree : List JsonNode -> Tree
 getTree jsonNodes =
     List.foldl insert Empty jsonNodes
 
+getJson : Maybe String -> Cmd Msg
+getJson maybeFileName =
+    case maybeFileName of
+        Nothing ->
+            Cmd.none
+        Just fileName ->
+            Http.send JsonReceived (Http.get fileName jsonNodesDecoder)
 
-getJson : String -> Cmd Msg
-getJson fileName =
-    Http.send JsonReceived (Http.get fileName jsonNodesDecoder)
+listFiles : Cmd Msg
+listFiles =
+    Http.send FileList (Http.get "list_files" (JD.list JD.string))
 
 rebuildSource : Cmd Msg
 rebuildSource =
--- flask-server.py
+++ flask-server.py
@@ -1,4 +1,7 @@
+import os
+from flask import jsonify
 import subprocess
+
 from flask import Flask, send_from_directory
 app = Flask(__name__, static_url_path="")
 
@@ -8,8 +11,15 @@ def main_page():
 
 @app.route('/rebuild_source')
 def rebuild_source():
-    subprocess.call(["elm-make", "Main.elm", "--output=static/main.js"])
+    subprocess.call(["elm-make", "Main.elm", "--output=static/index.html"])
     return ""
+
+@app.route('/list_files')
+def list_files():
+    files = [f for f in os.listdir('static') if f.endswith('.json')]
+    j = jsonify(files)
+    print(j.get_data().decode())
+    return j
 
 @app.route('/<path:path>')
 def serve_page(path):
